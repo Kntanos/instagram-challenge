d62cbeee935c7c969780cc998ebfeeb152a39f0b
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-11-13 21:34:10 +0000

    Added image cards on main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @posts = Post.all
+    @comments = Comment.all
+    @likes = Like.all
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_one_attached :post_image
+  has_one_attached :image
   has_many :comments
   has_many :likes
 end
