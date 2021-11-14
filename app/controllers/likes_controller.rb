5e4e5e11cec98cef58941cc0da8e74a189af4c47
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-11-14 15:13:29 +0000

    Added user name to post and user name to comments

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -26,7 +26,7 @@ class LikesController < ApplicationController
 
     respond_to do |format|
       if @like.save
-        format.html { redirect_to @like, notice: "Like was successfully created." }
+        format.html { redirect_to root_path } #, notice: "Like was successfully created." }
         format.json { render :show, status: :created, location: @like }
       else
         format.html { render :new, status: :unprocessable_entity }
